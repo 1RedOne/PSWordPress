a55c260cb47c7ae363064e7e7116857d6aa0b68b
FoxDeploy <sred13@gmail.com>
2015-09-10 15:21:25 -0400

    Almost all data in its own functions now

--- Public/SplitThis.ps1
+++ Public/SplitThis.ps1
@@ -10,21 +10,4 @@ $clientID = '37880'
 $blogURL  = 'http://www.foxdeploy.com'
 $username = 'sred13@gmail.com'
 
-Function Test-WordPressToken{
-param($clientID=$global:clientID,$authtoken=$global:AuthToken)
-    try {
-        $result =Invoke-RestMethod "https://public-api.wordpress.com/oauth2/token-info?client_id=$clientID&token=$authToken" -ErrorAction STOP
-        $err=$false
-    }
-    catch {
-        Write-Warning "Double check that you're providing a Token, and not an access code";$err=$true}
-    finally {
-        if (-not($err)){
-            Write-host "Token is valid, for the following user"
-            $result
-            }
-    }
-}
-
-
 
