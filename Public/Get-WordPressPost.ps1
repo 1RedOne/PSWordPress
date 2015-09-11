8e305f571f618855b9375e73608abc96d597ed97
Stephen Owen <sred13@gmail.com>
2015-09-11 12:24:06 -0400

    Added Get-WordPressPost
    Gets a list of all posts on your blog

---
+++ Public/Get-WordPressPost.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.Synopsis
+   Returns a list of all Posts, useful to send to other cmdlets
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Get-WordPressSite -domainName FoxDeploy.com | Get-WordPressPost | select -First 5 | ft -wrap
+Found 135 posts
+
+  ID Author    date                      title                      status  short_URL              Activity(Comments)
+  -- ------    ----                      -----                      ------  ---------              ------------------
+2489 FoxDeploy 2015-09-08T04:00:00-05:00 Part IV &#8211; PowerShell publish http://wp.me/p3Q7Nu-E9                  6
+2483 FoxDeploy 2015-08-25T10:56:44-05:00 &#8216;GUI your life with  publish http://wp.me/p3Q7Nu-E3                  0
+2454 FoxDeploy 2015-08-03T11:11:10-05:00 New in Windows 10: Changin publish http://wp.me/p3Q7Nu-DA                  2
+2216 FoxDeploy 2015-07-31T15:22:22-05:00 Recovering your Dedeuped F publish http://wp.me/p3Q7Nu-zK                 30
+
+Get basic information about the posts on your site, including comment activity
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+Function Get-WordPressPost {
+[CmdletBinding()]
+param([Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,
+                   Position=0)]$domainName,
+                   $accessToken=$Global:accessToken)
+
+
+$results = Invoke-RestMethod https://public-api.wordpress.com/rest/v1.1/sites/$domainName/posts -Method Get -Headers @{"Authorization" = "Bearer $accessToken"} 
+Write-output "Found $($results.found) posts"
+$results.posts | select ID,@{n='Author';Exp={$_.author.Name}},date,@{n='title';Exp={$_.title[0..25] -join ''}},status,short_URL,@{n='Activity(Comments)';Exp={$_.Discussion.Comment_Count}}
+
+}
--- Public/Get-WordPressSite.ps1
+++ Public/Get-WordPressSite.ps1
@@ -4,7 +4,18 @@
 .DESCRIPTION
    Long description
 .EXAMPLE
-   Example of how to use this cmdlet
+   Get-WordPressSite -domainName FoxDeploy.com
+
+
+ID                : 56752040
+name              : FoxDeploy.com
+description       : PowerShell tools and tool-making Tails from the fox hole
+URL               : http://foxdeploy.com
+post_count        : 184
+subscribers_count : 15
+DomainName        : FoxDeploy.com
+
+Get basic information about your site, useful for piping into another cmdlet
 .EXAMPLE
    Another example of how to use this cmdlet
 #>
@@ -15,6 +26,6 @@ param([Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,
                    $accessToken=$Global:accessToken)
 
 
-Invoke-RestMethod https://public-api.wordpress.com/rest/v1.1/sites/$domainName -Method Get -Headers @{"Authorization" = "Bearer $accessToken"} | select *,@{Name='DomainName';Exp={$domainName}}
+Invoke-RestMethod https://public-api.wordpress.com/rest/v1.1/sites/$domainName -Method Get -Headers @{"Authorization" = "Bearer $accessToken"} | select ID,name,Description,URL,post_count,subscribers_count,@{Name='DomainName';Exp={$domainName}} -ExcludeProperty options
 
 }
