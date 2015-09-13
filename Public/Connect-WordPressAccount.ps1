d46ba65752f936443cf0beacb9a840890fee9e9c
Stephen Owen <sred13@gmail.com>
2015-09-13 14:28:28 -0400

    Added help to Connect-WordPressAccount

--- Public/Connect-WordPressAccount.ps1
+++ Public/Connect-WordPressAccount.ps1
@@ -1,4 +1,22 @@
-﻿Function Connect-WordPressAccount {
+﻿<#
+.Synopsis
+    Use this cmdlet to connect to a WordPress account for management via PowerShell
+.DESCRIPTION
+    With one cmdlet, you can connect to your WordPress account via the REST API.  After using this cmdlet, you can use the rest of the cmdlets in the PSWordPress PowerShell module to Get-WordPressStats, make a New-WordPressPost or other activities
+.EXAMPLE
+    Connect-WordPressAccount -ClientID [String] -BlogURL [(Redirect URL)] -ClientSecret [ClientSecret]
+
+    Sign up at developer.wordpress.com and make a new application ID, which is needed to query to WordPress API.  While you're there, specify a redirect URL, which should be the URL of your blog.  You'll receive a ClientID, ClientSecret and RedirectURL, which you must provide to this cmdlet.
+
+    Upon running, a Internet Explorer com object window will be displayed, prompting you to login and authorize your Application ID (PowerShell, effectively) to interact with your WordPress account.  Click the appropriate boxes, and then close the browser window when you see your blog displayed.
+
+    Behind the scenes, this cmdlet will retrieve an Access Token, convert it to an Authorization Token, and store it safely within your profile.  Other PSWordpress API calls require this Authorization token, and it will be automatically provided when needed.
+.EXAMPLE
+    Connect-WordpressAccount -Force
+
+    If you need to renew your API key (roughly once a month), then rerun the cmdlet with -Force
+#>
+Function Connect-WordPressAccount {
 param($ClientID,$blogURL,$clientSecret,[Switch]$force)
 
 
