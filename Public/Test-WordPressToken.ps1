48773007f26ba85632694c37e1447148f66c1937
Ryan Yates <ryan.yates@kilasuit.org>
2016-03-07 16:54:42 +0000

    Proper Placement of Comment based help

--- Public/Test-WordPressToken.ps1
+++ Public/Test-WordPressToken.ps1
@@ -1,13 +1,12 @@
-﻿<#
+﻿Function Test-WordPressToken{
+<#
 .Synopsis
    This is used to test the current token
 .DESCRIPTION
    Used to ensure that token stored is valid for the user
 .EXAMPLE
    Test-WordPressToken
-#>
-Function Test-WordPressToken{
-[CmdletBinding()]
+#>[CmdletBinding()]
 param($clientID=$global:clientID,$accessToken=$global:accessToken)
     try {
         $result =Invoke-RestMethod "https://public-api.wordpress.com/oauth2/token-info?client_id=$clientID&token=$accessToken" -ErrorAction STOP
