e19ed8920469b6be80d31f29e620b2776150168d
Ryan Yates <ryan.yates@kilasuit.org>
2016-03-07 16:54:03 +0000

    Proper placement of Comment based help

--- Public/New-WordPressPost.ps1
+++ Public/New-WordPressPost.ps1
@@ -1,4 +1,5 @@
-﻿<#
+﻿Function New-WordPressPost {
+<#
 .Synopsis
    Use this cmdlet to retrieve up-to-the-minute statistics from your Wordpress site
 .DESCRIPTION
@@ -35,7 +36,6 @@ You can also pipe the output of Get-WordPressSite into this cmdlet, rather than 
 Code for this module can always be found here on GitHub
 https://github.com/1RedOne/WordPress
 #>
-Function New-WordPressPost {
 [Cmdletbinding()]
 param(
     [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,
