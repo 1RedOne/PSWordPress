8982a3df80cdc86d05af4f8e85327868c9bc9ff4
Ryan Yates <ryan.yates@kilasuit.org>
2016-03-07 15:11:50 +0000

    Added in ability to return more than 20 items by adding NumberToReturn Parameter and setting to default to 20. Also Added in some additional logic to get the correct Title of A Post removing the HTML encoding and also returns the Full URL of the Post

--- Public/Get-WordPressPost.ps1
+++ Public/Get-WordPressPost.ps1
@@ -31,6 +31,7 @@ param(
 
 $results = Invoke-RestMethod https://public-api.wordpress.com/rest/v1.1/sites/$domainName/posts/?number=$NumberToReturn -Method Get -Headers @{"Authorization" = "Bearer $accessToken"} 
 Write-output "Found $($results.found) posts"
-$results.posts | select ID,@{n='Author';Exp={$_.author.Name}},date,@{n='title';Exp={$_.title[0..25] -join ''}},status,short_URL,@{n='Activity(Comments)';Exp={$_.Discussion.Comment_Count}}
+$results.posts | 
+select ID,@{n='Author';Exp={$_.author.Name}},date,@{n='title';Exp={[System.Web.HttpUtility]::HtmlDecode($_.title) -join ''}},status,URL,short_URL,@{n='Activity(Comments)';Exp={$_.Discussion.Comment_Count}}
 
 }
