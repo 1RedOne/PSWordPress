5e72bdeeadbc906a5a92d5d21c31fd017ee76c53
Ryan Yates <ryan.yates@kilasuit.org>
2016-03-07 16:06:17 +0000

    Added Comment Based Help & made this an advanced function

--- Public/Get-WordPressAccount.ps1
+++ Public/Get-WordPressAccount.ps1
@@ -1,4 +1,19 @@
-﻿Function Get-WordPressAccount {
+﻿<#
+.Synopsis
+    Use this cmdlet to retrive details on your WordPress account for management via PowerShell
+.DESCRIPTION
+    With one cmdlet, you can retrive the details about your WordPress account via the REST API.  
+.EXAMPLE
+    Get-WordPressAccount -Insights
+    
+    This will retrive the Additional properties that can be pulled back for your WordPress Account
+.EXAMPLE
+    Get-WordPressAccount
+
+    This will retrive the Standard properties for your WordPress Account
+#>
+Function Get-WordPressAccount {
+[Cmdletbinding()]
 param($accessToken=$Global:accessToken,[switch]$Insights)
 
 if ($Insights){
